b2de520db4e910e6cd5180942fda8f54d2f988f4
hanks-zyh <zhangyuhan2014@gmail.com>
2015-11-05 19:01:40 +0800

    add models.py

---
+++ models.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+class TableItem(object):
+
+    def __init__(self, apk, description):
+        self.apk = apk
+        self.description = description
