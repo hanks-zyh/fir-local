07cbcc4f20ac9611493e26e2c193debc3ecb929c
hanks-zyh <zhangyuhan2014@gmail.com>
2015-11-09 15:06:53 +0800

    extra code form app.py to models.py

--- app/app.py
+++ app/app.py
@@ -4,48 +4,19 @@
 from flask import Flask
 from flask import send_from_directory
 from flask import render_template
-import os, time, stat
-from datetime import datetime
-from models import FileInfo
+from models import FileInfo, FileUtil, TableItem
 
 app = Flask(__name__)
 
-dirpath = os.path.join(app.root_path,'upload')
-
-
-def compare(file1, file2):
-    stat1 = os.stat(os.path.join(dirpath,file1))
-    stat2 = os.stat(os.path.join(dirpath,file2))
-    if stat1.st_ctime > stat2.st_ctime:
-        return -1
-    elif stat1.st_ctime < stat2.st_ctime:
-        return 1
-    else:
-        return 0
-
-def get_files():
-    apklist = []
-    filelist = os.listdir(dirpath)
-    filelist.sort(compare)
-    for f in filelist:
-        if  os.path.splitext(f)[1] =='.apk':
-            path = os.path.join(dirpath,f)
-            file_stat = os.stat(path)
-            size = '%.2f MB' % (float(file_stat.st_size)/1024/1024)
-            ctime = datetime.fromtimestamp(file_stat.st_ctime).strftime('%Y-%m-%d %H:%M:%S')
-            fileinfo = FileInfo(f, path, size, ctime)
-            apklist.append(fileinfo)
-    return apklist
-
 @app.route('/')
 def index():
-    apklist = get_files()
+    apklist = FileUtil.get_files()
     print apklist
     return render_template('index.html', apklist=apklist)
 
 @app.route('/apk/<path:filename>')
 def download(filename):
-    return send_from_directory(dirpath,filename,as_attachment=True)
+    return send_from_directory(FileUtil.get_upload_path() ,filename,as_attachment=True)
 
 if __name__ == '__main__':
     app.debug = True
--- app/models.py
+++ app/models.py
@@ -1,17 +1,63 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import os
+from flask import Flask
+import os, time, stat
+from datetime import datetime
+
+app = Flask(__name__)
+
+class FileUtil(object):
+    """
+    util class for file operator
+    """
+    @staticmethod
+    def get_upload_path():
+        return os.path.join(app.root_path,'upload')
+
+    @staticmethod
+    def get_files():
+        apklist = []
+        dirpath = FileUtil.get_upload_path()
+        filelist = os.listdir(dirpath)
+        filelist.sort(FileInfo.compare)
+        for f in filelist:
+            if  os.path.splitext(f)[1] =='.apk':
+                path = os.path.join(dirpath,f)
+                file_stat = os.stat(path)
+                size = '%.2f MB' % (float(file_stat.st_size)/1024/1024)
+                ctime = datetime.fromtimestamp(file_stat.st_ctime).strftime('%Y-%m-%d %H:%M:%S')
+                fileinfo = FileInfo(f, path, size, ctime)
+                apklist.append(fileinfo)
+        return apklist
+
+
 class FileInfo(object):
-    """include file name, path, size, ctime"""
+    """
+    include file name, path, size, ctime
+    """
     def __init__(self, name, path, size, ctime):
         self.name = name
         self.path = path
         self.size = size
         self.ctime = ctime
 
+    @staticmethod
+    def compare(file1, file2):
+        dirpath = FileUtil.get_upload_path()
+        stat1 = os.stat(os.path.join(dirpath,file1))
+        stat2 = os.stat(os.path.join(dirpath,file2))
+        if stat1.st_ctime > stat2.st_ctime:
+            return -1
+        elif stat1.st_ctime < stat2.st_ctime:
+            return 1
+        else:
+            return 0
+
+
 class TableItem(object):
     """descripte a tableitem in table"""
     def __init(self,fileinfo,description):
         self.fileinfo = fileinfo
         self.description = description
-        
