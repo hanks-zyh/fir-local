2d4f8227adfc898abc24b83490960bd974bfaa47
hanks-zyh <zhangyuhan2014@gmail.com>
2015-11-16 11:49:33 +0800

    add flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,17 @@
 __author__ = 'hanks'
 
 from flask import Flask
+from flask.ext.bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
 
 def create_app():
     app = Flask(__name__)
 
+    bootstrap.init_app(app)
+    
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     app.debug = True
     return app
-
