bdb97f228c8b55a3403cc75906ddf8a342facda8
hanks-zyh <zhangyuhan2014@gmail.com>
2015-11-16 11:13:11 +0800

    add base.html

--- manage.py
+++ manage.py
@@ -3,9 +3,12 @@
 
 from flask import Flask
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+
 from app import create_app
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 manage = Manager(create_app())
 
 if __name__ == '__main__':
