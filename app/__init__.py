e837b44c8ec4d352179e1ad83ba0f16c28b18654
hanks-zyh <zhangyuhan2014@gmail.com>
2015-11-27 16:36:27 +0800

    update td width

--- app/__init__.py
+++ app/__init__.py
@@ -12,9 +12,9 @@ def create_app():
     app = Flask(__name__)
 
     bootstrap.init_app(app)
-    
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    app.debug = True
+    #app.debug = True
     return app
