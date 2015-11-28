064f644286c5c94288627ef5d4beb45406ac4770
hanks-zyh <zhangyuhan2014@gmail.com>
2015-11-28 17:46:24 +0800

    clean popover code in html

--- app/static/js/woo.js
+++ app/static/js/woo.js
@@ -1,6 +1,8 @@
 function showQrcode(qrindex, qrpath) {
     $('#qr' + qrindex).popover({
         html: true,
+        trigger:'hover',
+        placement:'top',
         content: function () {
             return "<img src=\"" + qrpath + "\" width=\"250px\" height=\"250px\"/>";
         }
