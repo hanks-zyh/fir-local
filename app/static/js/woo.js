4def54475752417398b9e16e7eec52fcaa21c5f5
hanks-zyh <zhangyuhan2014@gmail.com>
2015-11-28 17:44:41 +0800

    clean popover code in html

--- app/static/js/woo.js
+++ app/static/js/woo.js
@@ -1,11 +1,8 @@
-function showQrcode(loopindex,qrpath){
-    $('#qr'+loopindex).popover({
-        html : true,
-        content : function(){
-            return "<img src=\""+qrpath+"\" width=\"250px\" height=\"250px\"/>";
+function showQrcode(qrindex, qrpath) {
+    $('#qr' + qrindex).popover({
+        html: true,
+        content: function () {
+            return "<img src=\"" + qrpath + "\" width=\"250px\" height=\"250px\"/>";
         }
     });
-}
-function hideQrcode(){
-    $('#qr').popover('hide');
 }
